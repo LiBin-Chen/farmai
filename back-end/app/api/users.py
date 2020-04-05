cd18152b7c4b6576bf02c966ac5d6e0e1fdf5e6a
ptrychen@gmail.com <1238023binbin>
2020-04-05 11:56:30 +0800

    修复user文件

--- back-end/app/api/users.py
+++ back-end/app/api/users.py
@@ -1,3 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+'''程序
+
+@description
+    说明
+'''
 from datetime import datetime
 from operator import itemgetter
 import re
@@ -9,15 +18,7 @@ from app.api.errors import bad_request, error_response
 from app.extensions import db
 from app.models import Permission, comments_likes, posts_likes, User, Post, Comment, Notification, Message, Task
 from app.utils.email import send_email
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
-
-'''程序
-
-@description
-    说明
-'''
 from app.utils.decorator import permission_required
 
 
